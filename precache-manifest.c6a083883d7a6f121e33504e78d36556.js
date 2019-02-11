dba2d1961f438866b65823885ecaea43e1f62e82
GenesisMauries <exodo_54@hotmail.com>
2019-02-11 12:58:37 -0600

    Updates

---
+++ precache-manifest.c6a083883d7a6f121e33504e78d36556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-one/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ed98f2b6440e866e5efb",
+    "url": "/react-one/static/js/runtime~main.ed98f2b6.js"
+  },
+  {
+    "revision": "fa0905e8a61e6759ddb0",
+    "url": "/react-one/static/js/main.fa0905e8.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/react-one/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "fa0905e8a61e6759ddb0",
+    "url": "/react-one/static/css/main.85d75cc9.chunk.css"
+  },
+  {
+    "revision": "23a617f636c688f34524bf520b204d04",
+    "url": "/react-one/index.html"
+  }
+];
